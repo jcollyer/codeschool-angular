fef9f1f81e89140de0362b2a4af1b6f7d9c03c2d
jcollyer <collyerdesign@gmail.com>
2014-06-08 18:52:01 -0700

    add submit form functionality

--- js/app.js
+++ js/app.js
@@ -95,4 +95,14 @@
     };
   });
 
+ app.controller('ReviewController', function(){
+    this.review = {};
+    this.addReview = function(product){
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
+
+
 })();
